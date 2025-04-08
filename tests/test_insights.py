5c3f7f206644f4c822963c6164a5a532815b9b2f
Mano Prakash Parthasarathi <mpartha@ncsu.edu>
2025-04-08 19:14:40 -0400

    Added test cases for burnout insights

--- tests/test_insights.py
+++ tests/test_insights.py
@@ -125,9 +125,6 @@ def test_non_zero_course_completion_rate(client):
     # Make a GET request to the /insights route
     insight_response = client.get('/insights')
 
-    with open("response_2.html", "w") as file:
-        file.write(str(insight_response.data))
-
     # As of now, the user has enrolled in one course, and completed it.
     # Assert the corresponding message for not completing any of the enrolled courses
     assert b"Completed 100.0% of the courses enrolled in" in insight_response.data
@@ -138,7 +135,7 @@ def test_non_zero_course_completion_rate(client):
 
 def test_get_max_calorie_intake(client):
     """
-        Test case for when the user has logged their calorie intake, one should be able to retrieve the max calorie intake in a day
+        Test case for when the user has logged their calorie intake, one should be able to retrieve the max calorie intake in a day.
     """
     # Simulate a logged-in user by setting session data
     with client.session_transaction() as sess:
@@ -165,7 +162,7 @@ def test_get_max_calorie_intake(client):
 
 def test_get_min_calorie_intake(client):
     """
-        Test case for when the user has logged their calorie intake, one should be able to retrieve the min calorie intake in a day
+        Test case for when the user has logged their calorie intake, one should be able to retrieve the min calorie intake in a day.
     """
     # Simulate a logged-in user by setting session data
     with client.session_transaction() as sess:
@@ -190,9 +187,10 @@ def test_get_min_calorie_intake(client):
     # Get rid of everything related to this user
     mongo.db.calories.delete_many({"email": user["email"]})
 
-def test_get_avg_calorie_intake(client):
+def test_get_avg_calorie_intake_1(client):
     """
-        Test case for when the user has logged their calorie intake, one should be able to retrieve the min calorie intake in a day
+        Test case for when the user has logged their calorie intake, one should be able to retrieve the average calorie intake in a day.
+        In this case, we expect a 'Recommended 2000 calories in a day' message 
     """
     # Simulate a logged-in user by setting session data
     with client.session_transaction() as sess:
@@ -211,11 +209,21 @@ def test_get_avg_calorie_intake(client):
     # Make a GET request to the /insights route
     response = client.get('/insights')
 
-    with open("response.html", "w") as file:
-        file.write(str(response.data)) 
-
     assert b"1101" in response.data
     assert b"Recommended 2000 calories in a day" in response.data
+
+    # Get rid of everything related to this user
+    mongo.db.calories.delete_many({"email": user["email"]})
+
+def test_get_avg_calorie_intake_2(client):
+    """
+        Test case for when the user has logged their calorie intake, one should be able to retrieve the average calorie intake in a day.
+        In this case, we expect a 'always good to maintain around 2000 calories per day' message. 
+    """
+    # Simulate a logged-in user by setting session data
+    with client.session_transaction() as sess:
+        user = mock_user()
+        sess['email'] = user["email"]
 
     # Get rid of everything related to this user
     mongo.db.calories.delete_many({"email": user["email"]})
@@ -226,12 +234,115 @@ def test_get_avg_calorie_intake(client):
     # Insert test data ()
     insert_test_calorie_burnout_data(data_file_name)
 
-        # Make a GET request to the /insights route
+    # Make a GET request to the /insights route
     response = client.get('/insights')
-
-    with open("response.html", "w") as file:
-        file.write(str(response.data)) 
 
     assert b"2013" in response.data
     assert b"always good to maintain around 2000 calories per day" in response.data
 
+def test_get_max_burnout(client):
+    """
+        Test case for when the user has logged their calorie intake, one should be able to retrieve the max burnout in a day.
+    """
+    # Simulate a logged-in user by setting session data
+    with client.session_transaction() as sess:
+        user = mock_user()
+        sess['email'] = user["email"]
+
+    # Get rid of everything related to this user
+    mongo.db.calories.delete_many({"email": user["email"]})
+
+    # JSON data file name
+    data_file_name = "test_max_min_calorie_burnout_data.json"
+    
+    # Insert test data
+    insert_test_calorie_burnout_data(data_file_name)
+    
+    # Make a GET request to the /insights route
+    response = client.get('/insights')
+
+    assert b"3000" in response.data
+    assert b"On April 01, 2025" in response.data
+
+    # Get rid of everything related to this user
+    mongo.db.calories.delete_many({"email": user["email"]})
+
+def test_get_min_burnout(client):
+    """
+        Test case for when the user has logged their calorie intake, one should be able to retrieve the min burnout in a day.
+    """
+    # Simulate a logged-in user by setting session data
+    with client.session_transaction() as sess:
+        user = mock_user()
+        sess['email'] = user["email"]
+
+    # Get rid of everything related to this user
+    mongo.db.calories.delete_many({"email": user["email"]})
+
+    # JSON data file name
+    data_file_name = "test_max_min_calorie_burnout_data.json"
+    
+    # Insert test data
+    insert_test_calorie_burnout_data(data_file_name)
+    
+    # Make a GET request to the /insights route
+    response = client.get('/insights')
+
+    assert b"180" in response.data
+    assert b"On March 26, 2025" in response.data
+
+    # Get rid of everything related to this user
+    mongo.db.calories.delete_many({"email": user["email"]})
+
+def test_get_avg_burnout_1(client):
+    """
+        Test case for when the user has logged their burnout, one should be able to retrieve the average burnout in a day.
+        In this case, we expect a 'Recommended a burnout of about 2000 calories in a day' message.
+    """
+    # Simulate a logged-in user by setting session data
+    with client.session_transaction() as sess:
+        user = mock_user()
+        sess['email'] = user["email"]
+
+    # Get rid of everything related to this user
+    mongo.db.calories.delete_many({"email": user["email"]})
+
+    # JSON data file name
+    data_file_name = "test_max_min_calorie_burnout_data.json"
+    
+    # Insert test data (Recommended 2000 calories in a day)
+    insert_test_calorie_burnout_data(data_file_name)
+    
+    # Make a GET request to the /insights route
+    response = client.get('/insights')
+
+    assert b"1257" in response.data
+    assert b"Recommended a burnout of about 2000 calories in a day" in response.data
+
+    # Get rid of everything related to this user
+    mongo.db.calories.delete_many({"email": user["email"]})
+
+def test_get_avg_burnout_2(client):
+    """
+       Test case for when the user has logged their burnout, one should be able to retrieve the average burnout in a day.
+        In this case, we expect a 'always good to maintain around 2000 calories per day' message.
+    """
+    # Simulate a logged-in user by setting session data
+    with client.session_transaction() as sess:
+        user = mock_user()
+        sess['email'] = user["email"]
+
+    # Get rid of everything related to this user
+    mongo.db.calories.delete_many({"email": user["email"]})
+
+    # JSON data file name
+    data_file_name = "test_good_avg_calorie_burnout_data.json"
+    
+    # Insert test data ()
+    insert_test_calorie_burnout_data(data_file_name)
+
+    # Make a GET request to the /insights route
+    response = client.get('/insights')
+
+    assert b"2500" in response.data
+    assert b"doing great !" in response.data
